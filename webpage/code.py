71829ef38e2ded10a49802538e7fb16365f2c463
hefl99 <florian.hetzel1@gmail.com>
2023-04-18 18:55:58 +0100

    website

--- webpage/code.py
+++ webpage/code.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+
 app = Flask(__name__)
 
-with(open("webpage\\terms.json", encoding="utf-8") as file):
+with open("webpage\\terms.json", encoding="utf-8") as file:
     db = json.load(file)
 
 keys = list(db.keys())
@@ -13,15 +14,25 @@ keys = sorted(keys, key=str.casefold)
 def home():
     return render_template("home.html")
 
-
 @app.route("/terms")
 def terms():
     return render_template("terms.html", designations=keys)
-
 
 @app.route("/term/<t>")
 def term(t):
     return render_template("term.html", designation = t, value= db.get(t,"None"))
 
-app.run(host="localhost", port=3000, debug=False)
+@app.route("/search")
+def search():
+    search_term = request.args.get('user_input')
 
+    results = []
+    if search_term:
+        for key in db:
+            if search_term.casefold() in key.casefold():
+                results.append(key)
+
+    return render_template("search.html", results=results, search_term=search_term)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=3000, debug=False)
