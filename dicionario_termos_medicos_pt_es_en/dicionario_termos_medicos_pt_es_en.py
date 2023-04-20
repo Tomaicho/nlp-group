5a8d2c6879ad4d236f2db8d67c2a9c000b2373fb
hefl99 <florian.hetzel1@gmail.com>
2023-04-20 11:08:47 +0100

    added comments

--- dicionario_termos_medicos_pt_es_en/dicionario_termos_medicos_pt_es_en.py
+++ dicionario_termos_medicos_pt_es_en/dicionario_termos_medicos_pt_es_en.py
@@ -4,18 +4,18 @@ import json
 with(open("XMLs/dicionario_termos_medicos_pt_es_en.xml", encoding="utf-8") as file):
     xml = file.read()
 
-new_xml = re.sub(r"<text.+?>","",xml)                       
-new_xml = re.sub(r"</text>","", new_xml)
-new_xml = re.sub(r"<page.+>","", new_xml)
-new_xml = re.sub(r"(<b>.+</b>\n)+</page>",r"", new_xml)     #remove side notch(portugues-ingles-espanhol) and </page>
-new_xml = re.sub(r"<b>\d+</b>","", new_xml)                 #remove page number
-new_xml = re.sub(r"<b>[A-Z]</b>","", new_xml)               #remove big letters in the beggining of each chapter
-new_xml = re.sub(r"\n<i>.+</i>","", new_xml)
-new_xml = re.sub(r"</b>\n<b>"," ", new_xml)
-new_xml = re.sub(r"-\n","", new_xml) 
-new_xml = re.sub(r"\n\n","\n", new_xml) 
-new_xml = re.sub(r"\(\n?[^\)]*\n?\+\n?[^\)]*\n?\)","", new_xml)
-new_xml = re.sub(r"</b>","", new_xml)
+new_xml = re.sub(r"<text.+?>","",xml)                           #remove text flags
+new_xml = re.sub(r"</text>","", new_xml)                        #remove end text flags
+new_xml = re.sub(r"<page.+>","", new_xml)                       #remove page flags
+new_xml = re.sub(r"(<b>.+</b>\n)+</page>",r"", new_xml)         #remove side notch(portugues-ingles-espanhol) and </page>
+new_xml = re.sub(r"<b>\d+</b>","", new_xml)                     #remove page number
+new_xml = re.sub(r"<b>[A-Z]</b>","", new_xml)                   #remove big letters in the beggining of each chapter
+new_xml = re.sub(r"\n<i>.+</i>","", new_xml)                    #remove italic gender declaration
+new_xml = re.sub(r"</b>\n<b>"," ", new_xml)                     #merge splitted term declarations from two lines in one
+new_xml = re.sub(r"-\n","", new_xml)                            #merge words/paragraphs splitted by -
+new_xml = re.sub(r"\n\n","\n", new_xml)                         #remove empty lines
+new_xml = re.sub(r"\(\n?[^\)]*\n?\+\n?[^\)]*\n?\)","", new_xml) #remove scaffolds from (adj. + m)
+new_xml = re.sub(r"</b>","", new_xml)                           #remove end bold flag
 
 
 with(open("XMLs/dicionario_termos_medicos_pt_es_en_new.xml", "w",  encoding="utf-8") as new_file):
