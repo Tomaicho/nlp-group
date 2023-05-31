42f5d9c0c22a757429a764f16ecefd3649d71e34
Tomaicho <tomas.a.lima@gmail.com>
2023-06-01 00:40:06 +0100

    relations in desc run to terms_v9

--- Second Assignement/Make relations/relations_desc.py
+++ Second Assignement/Make relations/relations_desc.py
@@ -7,7 +7,11 @@ def find_related_terms(data):
     for term in data:
         if "des_en" in data[term]:
             descriptions = data[term]["des_en"]
-            relations = data[term]["relations"]
+            if 'relations' in data[term]:
+                relations = data[term]['relations']
+            else:
+                relations = []
+                data[term]['relations'] = relations
             nouns = []
             adjectives = []
 
@@ -15,28 +19,40 @@ def find_related_terms(data):
                 doc = nlp(description)
                 for token in doc:
                     if token.pos_ == "NOUN":  # Noun check
-                        nouns.append(token.lemma_.lower())
+                        nouns.append(token.text.lower())
                     elif token.pos_ == "ADJ":  # Adjective check
-                        adjectives.append(token.lemma_.lower())
+                        adjectives.append(token.text.lower())
 
             for noun in nouns:
                 if noun in data and noun not in relations and noun != term:
-                    data[term]["relations"].append(noun)
-                    data[noun]["relations"].append(term)
+                    relations.append(noun)
+                    if 'relations' in data[noun]:
+                        relations2 = data[noun]['relations']
+                        print(relations2)
+                        relations2.append(term)
+                        data[noun]["relations"] = relations2
+                    else:
+                        data[noun] = {'relations': term}
 
             for adj in adjectives:
                 if adj in data and adj not in relations and adj != term:
-                    data[term]["relations"].append(adj)
-                    data[adj]["relations"].append(term)
+                    relations.append(adj)
+                    if 'relations' in data[adj]:
+                        relations2 = data[adj]['relations']
+                        relations2.append(term)
+                        data[adj]["relations"] = relations2
+                    else:
+                        data[adj] = {'relations': term}
+            data[term]["relations"] = (relations)
 
     return data
 
-with open('C:/Users/tomas/Desktop/Mestrado/1A_2S/Processamento Linguagem Natural/nlp-group/Second assignement/jsons/test.json') as file:
+with open('C:/Users/tomas/Desktop/Mestrado/1A_2S/Processamento Linguagem Natural/nlp-group/Second assignement/jsons/terms_v8.json') as file:
     data = json.load(file)
 
 updated_data = find_related_terms(data)
 
-with open('C:/Users/tomas/Desktop/Mestrado/1A_2S/Processamento Linguagem Natural/nlp-group/Second assignement/jsons/test2.json','w') as file:
+with open('C:/Users/tomas/Desktop/Mestrado/1A_2S/Processamento Linguagem Natural/nlp-group/Second assignement/jsons/terms_v9.json','w') as file:
     json.dump(updated_data, file, indent=6, ensure_ascii=False)
 
 
