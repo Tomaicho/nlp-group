00b420ef429ede14c86e446e5d580c3160e9808d
hefl99 <72932145+hefl99@users.noreply.github.com>
2023-06-01 14:00:55 +0100

    lhv

--- Second Assignement/Website/code.py
+++ Second Assignement/Website/code.py
@@ -3,7 +3,7 @@ import json
 
 app = Flask(__name__)
 
-with open("./trabalho-grupo/webpage/terms.json", encoding="utf-8") as file:
+with open("Website/terms_v9.json") as file:
     db = json.load(file)
 
 keys = list(db.keys())
@@ -20,7 +20,15 @@ def terms():
 
 @app.route("/term/<t>")
 def term(t):
-    return render_template("term.html", designation = t, value= db.get(t,"None"))
+    relations = []
+    dic = db.get(t)
+    if dic.get('relations') is not None:
+        relations = dic.get('relations')
+        relations_in_db = [term for term in relations if term in db]
+        for relation in relations_in_db:
+            relations.append(relation)
+    
+    return render_template("term.html", designation = t, relations=relations, value= db.get(t,"None"))
 
 @app.route("/search")
 def search():
@@ -37,5 +45,28 @@ def search():
 
     return render_template("search.html", results=results, search_term=search_term)
 
+@app.route('/categories/<term>')
+def categories(term):
+    important_terms = ['anatomy', 'bacteria', 'blood', 'cancer', 'cell', 'diagnosis', 'disease', 'dna', 'drug', 'epidemic',
+    'fever', 'gene', 'heart', 'hormone', 'immune', 'infection', 'inflammation', 'injury', 'kidney', 'liver',
+    'lung', 'microscope', 'nerve', 'organ', 'pain', 'pathology', 'patient', 'pharmacy', 'physician', 'radiation',
+    'research', 'surgery', 'symptom', 'therapy', 'tissue', 'treatment', 'vaccine', 'virus', 'wound']
+    important_terms_in_db = [term for term in important_terms if term in db]
+    new_relations = set()
+    if term =='base':
+        new_relations = set(important_terms_in_db)
+    else:
+        dic = db.get(term)
+        if dic.get('relations') is not None:
+            relations = dic.get('relations')
+            relations_in_db = [term for term in relations if term in db]
+            for relation in relations_in_db:
+                new_relations.add(relation)
+    
+    new_relations = sorted(list(new_relations))
+        
+    return render_template('categories.html', terms = new_relations)
+
 if __name__ == '__main__':
     app.run(host="localhost", port=3000, debug=False)
+
